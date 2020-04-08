cc50c57b168aa2ae1c1762e6422d391868cbc0fe
KhadijaMahanga <khadijamahanga@y7mail.com>
2020-04-08 11:23:15 +0300

    country codetype command

---
+++ mapit/management/commands/mapit_add_country_codetype.py
@@ -0,0 +1,44 @@
+# This script is for a one off import of all the old ONS codes to a MapIt
+# containing only the new ones from a modern Boundary-Line.
+
+import codecs
+import csv
+import os.path
+import sys
+from django.core.management.base import BaseCommand
+from mapit.models import Area, CodeType
+from psycopg2 import IntegrityError
+
+python_version = sys.version_info[0]
+
+
+def open_csv(filename):
+    if python_version < 3:
+        o = open(filename)
+    else:
+        o = codecs.open(filename, 'r', 'utf-8')
+    mapping = csv.reader(o)
+    next(mapping)
+    return mapping
+
+
+def process(afr_code, c_code, c_codetype):
+    try:
+        area = Area.objects.get(codes__code=afr_code, codes__type__code='AFR')
+    except Area.DoesNotExist:
+        # An area that existed at the time of the mapping, but no longer
+        return
+
+    try:
+        area.codes.create(type=CodeType.objects.get(code=c_codetype), code=c_code)
+    except IntegrityError:
+        raise Exception("Key already exists for %s, can't give it %s" % (area, c_code))
+
+
+class Command(BaseCommand):
+    help = 'Inserts country specific codes into mapit'
+
+    def handle(self, **options):
+        for row in open_csv(os.path.join(os.path.dirname(__file__), '../geo/countrygeoid.csv')):
+            afr_code, c_codetype, c_code = row[0], row[1], row[2]
+            process(afr_code, c_code, c_codetype)

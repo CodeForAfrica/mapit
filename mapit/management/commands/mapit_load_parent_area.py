fee2c288b0c9dd18d8d24a10370cb2c1d2d95bcf
KhadijaMahanga <khadijamahanga@y7mail.com>
2020-04-09 18:26:43 +0300

    self in function

--- mapit/management/commands/mapit_load_parent_area.py
+++ mapit/management/commands/mapit_load_parent_area.py
@@ -21,34 +21,6 @@ def open_csv(filename):
     return mapping
 
 
-def process(area_code, parent_area_code):
-    parent = None
-    try:
-        area = Area.objects.get(codes__code=area_code, codes__type__code='AFR')
-    except Area.DoesNotExist:
-        # An area that existed at the time of the mapping, but no longer
-        return
-
-    try:
-        parent = Area.objects.get(codes__code=parent_area_code, codes__type__code='AFR')
-    except Area.DoesNotExist:
-        # parent_area does not exist
-        pass
-
-    if not parent:
-        raise Exception("Area %s does not have a parent?" % (self.pp_area(area)))
-    if area.parent_area != parent:
-        self.stdout.write("Parent for %s was %s, is now %s" % (
-            self.pp_area(area), self.pp_area(area.parent_area), self.pp_area(parent)))
-
-        area.parent_area = parent
-        area.save()
-
-def pp_area(self, area):
-        if not area:
-            return "None"
-        return "%s [%d] (%s)" % (area.name, area.id, area.type.code)
-
 class Command(BaseCommand):
     help = 'Load parent area'
 
@@ -57,4 +29,29 @@ class Command(BaseCommand):
             area_code = row[0]
             parent_area_code = row[1]
 
-            process(area_code, parent_area_code)
+            parent = None
+            try:
+                area = Area.objects.get(codes__code=area_code, codes__type__code='AFR')
+            except Area.DoesNotExist:
+                # An area that existed at the time of the mapping, but no longer
+                return
+
+            try:
+                parent = Area.objects.get(codes__code=parent_area_code, codes__type__code='AFR')
+            except Area.DoesNotExist:
+                # parent_area does not exist
+                pass
+
+            if not parent:
+                raise Exception("Area %s does not have a parent?" % (self.pp_area(area)))
+            if area.parent_area != parent:
+                self.stdout.write("Parent for %s was %s, is now %s" % (
+                    self.pp_area(area), self.pp_area(area.parent_area), self.pp_area(parent)))
+
+                area.parent_area = parent
+                area.save()
+
+    def pp_area(self, area):
+        if not area:
+            return "None"
+        return "%s [%d] (%s)" % (area.name, area.id, area.type.code)
